93cb2494324b051a91445a48dc119e1c1faf982b
g-serban <gserban1233@gmail.com>
2022-02-17 22:03:38 +0200

    initial files

---
+++ workout_app/lib/home_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.homePageBackground,
+      body: Container(
+        padding: const EdgeInsets.only(top: 70, left: 30, right: 30),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text('Training',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: AppColor.homePageTitle,
+                      fontWeight: FontWeight.w700,
+                    )),
+                Expanded(child: Container()),
+                Icon(Icons.arrow_back_ios,
+                    size: 20, color: AppColor.homePageIcons),
+                const SizedBox(width: 7),
+                Icon(Icons.calendar_today_outlined,
+                    size: 20, color: AppColor.homePageIcons),
+                const SizedBox(width: 10),
+                Icon(Icons.arrow_forward_ios,
+                    size: 20, color: AppColor.homePageIcons),
+              ],
+            ),
+            const SizedBox(height: 30),
+            Row(
+              children: [
+                Text('Your program',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: AppColor.homePageSubtitle,
+                        fontWeight: FontWeight.w700)),
+                Expanded(child: Container()),
+                Text('Details',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: AppColor.homePageDetail,
+                        fontWeight: FontWeight.w700)),
+                const SizedBox(width: 5),
+                Icon(Icons.arrow_forward,
+                    size: 20, color: AppColor.homePageIcons),
+              ],
+            ),
+            // const SizedBox(height:20),
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              width: MediaQuery.of(context).size.width,
+              height: 220,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  AppColor.gradientFirst,
+                  AppColor.gradientSecond,
+                ]),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                  topRight: Radius.circular(80),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
